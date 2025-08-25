6ef8d5ed82236881cc75d1deda387339a49f09b0
carlosresendeP <carlosresende.paula@gmail.com>
2025-08-25 20:03:04 -0300

    Refactor code structure for improved readability and maintainability

--- interface/src/routes/index.tsx
+++ interface/src/routes/index.tsx
@@ -1,13 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/Home";
+import { Login } from "../pages/Login";
+import { AuthProvider } from "../context/AuthContext";
+import Dashboard from "../pages/Dashboard";
+import AppLayout from "../layout/AppLayout";
+import { PrivateRoutes } from "./PrivateRoutes";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<h2>Página não encontrada</h2>} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+
+          {/* Rotas Privadas */}
+          <Route element={<PrivateRoutes />}>
+            <Route element={<AppLayout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<h2>Página não encontrada</h2>} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
